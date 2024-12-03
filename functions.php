374d1fb0f596dcc4a4505e4fcf04e7f204cd5f47
thy8000 <thunaymoreiradesoares@gmail.com>
2024-12-02 23:09:16 -0300

    ➕ Composer/Tailwind & Composer autoload to classes namespaces

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ if (!defined('ABSPATH')) {
    exit;
 }
 
+include get_template_directory() . '/vendor/autoload.php';
+
 new NutriTrack\Core\EnqueueScripts();
 new NutriTrack\Core\TemplateHierarchy();
 new NutriTrack\Core\ThemeSupport();
